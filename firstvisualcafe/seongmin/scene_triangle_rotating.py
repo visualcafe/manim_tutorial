7085298fb22d78c37462910de6c5d00c4221b1ef
seongmincho <seongmincho33@gmail.com>
2021-10-30 11:09:42 +0900

    Update scene_triangle_rotating.py

--- firstvisualcafe/seongmin/scene_triangle_rotating.py
+++ firstvisualcafe/seongmin/scene_triangle_rotating.py
@@ -1,20 +1,26 @@
 from manim import *
 import numpy as np
 
+
+#속도개선 필요
+#트레이스 추가
+
 class TriangleRotating(Scene):
     def construct(self):
         #음.... 일단 삼각형과같이 다각형 클래스중에 이름있는것
         #예를들어 사각형 삼각형 이런것들은 수정하기가 어렵습니다.
         #따라서 polygon클래스를 이용해서 객체를 생성하는편이 좋은것 같습니다.
-        theta = PI / 7
+        
         triangle = Polygon(
             ORIGIN,
-            RIGHT*(1+2*np.cos(2*theta)),
-            RIGHT*(1+2*np.cos(2*theta)) + UP*2*np.sin(theta)*(np.cos(theta) + np.cos(3*theta))
-            )
+            RIGHT,
+            RIGHT/2 + UP*np.sin(PI/3)
+        )
         self.add(triangle)
-        self.play(Rotating(triangle, about_point=ORIGIN, radians=PI))
+        for i in range(4):
+            self.play(Rotating(triangle, about_point=LEFT*i, radians=2*PI/3))
+            
 
-if __name__ == "main":
+if __name__ == "__main__":
     scene = TriangleRotating()
     scene.render()
